6c148c4507b2d055e002bb6e4ae77d0347a53778
Heitor Urbanetz <jhty123453@gmail.com>
2022-11-29 17:03:57 -0300

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import { MongoClient } from 'mongodb'
-import { Sequelize, QueryTypes } from 'sequelize'
-import * as dotenv from 'dotenv'
+import { MigrateDatabase } from "./migrate.js"
 
-(async () => {
-    dotenv.config()
-    
-    const mongodb = new MongoClient('mongodb://localhost:27017')
-    const sequelize = new Sequelize({
-        host: process.env.HOST,
-        dialect: process.env.DIALECT,
-        database: process.env.DATABASE,
-        username: process.env.DBNAME,
-        password: process.env.PASSWORD
-    })
-    await sequelize.sync({ force: false })
-    const myResult = await sequelize.query('SELECT * FROM estado', { type: QueryTypes.SELECT })
-    const db = mongodb.db('testando')
-    const collection = db.collection('testando')
-    
-    if (myResult.length > 0) {
-        collection.insertMany(myResult)
-    } 
-})()
+const db = new MigrateDatabase()
+db.migrate('SELECT * FROM estado', 'testando', 'testando')
 
 
---
+++ migrate.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb'
+import { Sequelize, QueryTypes } from 'sequelize'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export class MigrateDatabase {
+    constructor() {
+        this.sequelize = new Sequelize({
+            host: process.env.HOST,
+            dialect: process.env.DIALECT,
+            database: process.env.DATABASE,
+            username: process.env.DBNAME,
+            password: process.env.PASSWORD
+        })
+        this.mongodb = new MongoClient(process.env.MONGOHOST)
+    }
+
+    async migrate(query, database, collection) {
+        await this.sequelize.sync({ force: false })
+        const myResult = await this.sequelize.query(query, { type: QueryTypes.SELECT })
+        const db = this.mongodb.db(database)
+        const collectionMongo = db.collection(collection)
+        
+        if (myResult.length > 0) {
+            collectionMongo.insertMany(myResult)
+        } 
+    }
+}
